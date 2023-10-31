e97c0abb72b69b196a0008240f988e5e2f44c25c
sarahkim4002 <76935205+sarahkim4002@users.noreply.github.com>
2023-10-31 13:46:17 -0400

    Update main.py

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ def main():
         print("1. Encode")
         print("2. Decode")
         print("3. Quit")
-        option = input("Please enter an option: ")
+        option = input("\nPlease enter an option: ")
         if option == "1":
             value = encode(input("Please enter your password to encode: "))
             print("Your password has been encoded and stored!")
